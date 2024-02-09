e1bd381e6f1bdd15886baa3e217cc5939f0b6fd5
Sandrikk <krevovasandra@gmail.com>
2024-02-09 21:22:25 +0100

    added skills part

--- src/components/SkillsSection.svelte
+++ src/components/SkillsSection.svelte
@@ -1,3 +1,14 @@
 <section class="container">
     <h2>Skills</h2>
+    <h3>Frontend</h3>
+    <p>HTML, CSS (Bootstrap), JavaScript (Svelte) </p>
+    <h3>Backend</h3>
+    <p>Node.js, Express.js, SQLite, PostgreSQL</p>
+    <h3>Apps</h3>
+    <p>Java</p>
+    <h3>Tools</h3>
+    <p>Git, Figma, Postman</p>
+    <h3>Generative Art</h3>
+    <p>Processing</p>
+
 </section>
