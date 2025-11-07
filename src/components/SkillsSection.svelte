3696948b37006cee876242d2b527e69d68cb94de
sandrakrevova <516044@student.saxion.nl>
2025-11-07 16:19:55 +0100

    updated skills

--- src/components/SkillsSection.svelte
+++ src/components/SkillsSection.svelte
@@ -1,13 +1,13 @@
 <section class="container">
     <h2>Skills</h2>
     <h3>Frontend</h3>
-    <p>HTML, CSS (Bootstrap), JavaScript (Svelte) </p>
+    <p>HTML, CSS (Bootstrap, Tailwind), JavaScript (Vue.js, Svelte) </p>
     <h3>Backend</h3>
-    <p>Node.js, Express.js, SQLite, PostgreSQL</p>
+    <p>Node.js, NestJS, PostgreSQL</p>
     <h3>Apps</h3>
     <p>Java</p>
     <h3>Tools</h3>
-    <p>Git, Figma, Postman</p>
+    <p>Git, Docker, Figma, Postman</p>
     <h3>Generative Art</h3>
     <p>Processing</p>
 
