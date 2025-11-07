49f2f0fc0be6101e0609fbc7aa2f4160e97ae180
sandrakrevova <516044@student.saxion.nl>
2025-11-07 16:29:17 +0100

    updated url

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -4,7 +4,6 @@
 
     import ProjectsSection from "../components/ProjectsSection.svelte";
     import SkillsSection from "../components/SkillsSection.svelte";
-    import CVSection from "../components/CVSection.svelte";
     import AboutMeSection from "../components/AboutMeSection.svelte";
 
     import image1 from '../assets/preview-image1.png'
@@ -15,7 +14,7 @@
 
     let projects = [
         { projectName: "RareReads", date: 2023, skillsUsed: "HTML, CSS, Svelte, Node.js, Express.js", previewImage: image2, projectLink: "https://github.com/sandrikk/auction-site", type: "web" },
-        { projectName: "NEWGROUNDAI", date: 2023, skillsUsed: "HTML, CSS, JavaScript, Three.js", previewImage: image1, projectLink: "https://newgroundai.com/", type: "web" },
+        { projectName: "NEWGROUNDAI", date: 2023, skillsUsed: "HTML, CSS, JavaScript, Three.js", previewImage: image1, type: "web", projectLink: "https://github.com/sandrikk/newgroundai" },
         { projectName: "SoundSculptor", date: 2023, skillsUsed: "HTML, CSS, JavaScript, Node.js, Express.js, SQLite", previewImage: image3, projectLink: "", type: "web" },
         { projectName: "TrackManager", date: 2023, skillsUsed: "Java", previewImage: image4, projectLink: "https://github.com/sandrikk/TrackManagerApp", type: "apps" },
         { projectName: "Lorenz System", date: 2022, skillsUsed: "Processing", previewImage: image5, projectLink: "https://openprocessing.org/sketch/1781876", type: "art" },
